b651db135b1cfa435c470c2e7d1db3ade80cbe41
Christa Kulanda <christakulanda@gmail.com>
2025-04-05 23:45:20 -0500

    second

--- Christa Kulanda Output 6/ContentView.swift
+++ Christa Kulanda Output 6/ContentView.swift
@@ -10,12 +10,25 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Look at this cute puppy!")
+                .font(.title)
+                .fontWeight(.bold)
+                .foregroundColor(Color.pink)
+            Image("cute puppy")
+                .resizable(resizingMode: .stretch)
+                .aspectRatio(contentMode: .fit)
+                
+            Button("Click Me!") {
+            print("Button tapped!")
+            }
+            .padding(.top, 20.0)
+            .buttonStyle(.borderedProminent)
+            .controlSize(.large)
+            .tint(.pink)
+            .font(.title2)
         }
-        .padding()
+        
+        
     }
 }
 
